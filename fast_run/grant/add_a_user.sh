7dcd1f94b8774a5ae5d55bc2567e143a7cec6982
LiPF <hudalpf@163.com>
2021-05-29 17:17:10 +0800

    grant

--- fast_run/grant/add_a_user.sh
+++ fast_run/grant/add_a_user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-user='chenqiren'
+user='lipengfei'
 key='chenqiren@504'
 echo "create user [$user]"
-useradd -d /home/$user -m $user
+groupadd -r $user
+useradd -m -r -g $user $user -d /home/$user
 echo $key | passwd --stdin $user
-sed -i "/^root/a\\${user} ALL = (ALL:ALL) NOPASSWD:ALL" /etc/sudoers
+sed -i "/^root/a\\${user} ALL = (ALL:ALL) ALL" /etc/sudoers
 usermod -aG docker ${user}
--- fast_run/grant/del_a_user.sh
+++ fast_run/grant/del_a_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-userdel -r chenqiren
-rm -rf /home/chenqiren
+userdel -r lipengfei
+rm -rf /home/lipengfei
